cc0eedcbf89e3e9f184e555c0686fd0079af7b9c
StxpAmir <bladimirgf17@gmail.com>
2025-11-11 08:26:07 -0600

    Add files via upload

---
+++ TrianguloFloyd.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class TrianguloFloyd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingresa la altura deseada del triángulo: ");
+        int n = sc.nextInt();
+        
+        int num = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
